b9c2fd1db3a813238784cf8d8e20c4c6cb892282
Milana <94888u@gmail.com>
2024-03-18 00:20:16 +0100

    implement inserting

---
+++ src/stages/inserting_in_oven/inserting_in_oven.gd
@@ -0,0 +1,30 @@
+extends StageBase
+
+
+var is_moving := false
+
+var finished_flag := false
+
+@onready var shovel := $Shovel as TextureButton
+
+
+func _on_shovel_button_down() -> void:
+	is_moving = true
+
+
+func _on_shovel_button_up() -> void:
+	is_moving = false
+
+
+func _process(delta: float) -> void:
+	if finished_flag:
+		return
+	if not is_moving:
+		return
+	if get_global_mouse_position().x <= shovel.position.x:
+		return
+	shovel.position.x += 100 * delta
+	shovel.position.x = minf(shovel.position.x, 1220)
+	if shovel.position.x > 1219:
+		finished_flag = true
+		StageManager.switch_to_next_stage()

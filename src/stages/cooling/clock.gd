3d2193fa20c39a32b06967fa361cacd45a49270c
Milana <94888u@gmail.com>
2024-03-18 05:17:42 +0100

    implement making soft

--- src/stages/cooling/clock.gd
+++ src/stages/cooling/clock.gd
@@ -11,7 +11,7 @@ func bell() -> void:
 	$Bell.playing = true
 	timeout.emit()
 
-func wait(seconds: float, progress_step = 0.1, pause_step = 0.5) -> void:
+func wait(seconds: float, pause_step = 0.5) -> void:
 	($Timer as Timer).start(pause_step)
 	var tween := create_tween()
 	tween.tween_property(arrow, "rotation_degrees", 0.0, seconds).from(90)
--- src/stages/making_soft/making_soft.gd
+++ src/stages/making_soft/making_soft.gd
@@ -9,3 +9,19 @@ func _on_out_pressed() -> void:
 	stages[-1].hide()
 	stages[0].show()
 	
+
+
+func _on_mash_1_pressed() -> void:
+	stages[0].hide()
+	stages[1].show()
+
+
+func _on_mash_2_pressed() -> void:
+	stages[1].hide()
+	stages[2].show()
+
+
+func _on_mash_3_pressed() -> void:
+	stages[2].hide()
+	stages[3].show()
+	StageManager.switch_to_next_stage()
